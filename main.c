d04cb0ee3f226f08b57029260f96d9dab47d171a
BramJ <12401325@student.pxl.be>
2025-11-20 10:37:18 +0100

    First commit with working blinking led

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
+#include "../work/pigpio.h"
+
+int main(void)
+{
+    static const unsigned int LED_GPIO_PIN = 26; 
+    gpioInitialise();
+
+    gpioSetMode(LED_GPIO_PIN, PI_OUTPUT);
+
+    printf("Blinking LED on GPIO %d...\n", LED_GPIO_PIN);
+
+    while (1)
+    {
+        gpioWrite(LED_GPIO_PIN, PI_ON); // LED ON
+        sleep(1);
+
+        gpioWrite(LED_GPIO_PIN, PI_OFF); // LED OFF
+        sleep(1);
+    }
+
+    gpioTerminate();   
+    return 0;
+}
